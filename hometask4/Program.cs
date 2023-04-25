41cd00419f57ae69e9982cdcb4a6ab443b8f61b6
ikova06 <ikova06#gmail.com>
2023-04-25 23:23:52 +0300

    Sem1_programming

---
+++ hometask4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число A ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB && numberA > numberC)
+{
+    Console.WriteLine($"Наибольшее число {numberA}");
+}
+if (numberB > numberA && numberB > numberC)
+{
+    Console.WriteLine($"Наибольшее число {numberC}");
+}
+if (numberC > numberB && numberC > numberA)
+{
+    Console.WriteLine($"Наибольшее число {numberC}");
+}
---
+++ task2/Program.cs
@@ -0,0 +1,85 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру. 3 -> Среда 5 -> Пятница
+
+Console.Write("Введите день: ");
+int day = Convert.ToInt32(Console.ReadLine());
+// if (day > 0 && day < 8)
+// {
+// if (day == 1)
+// {
+// Console.WriteLine("Понедельник");
+// }
+// if (day == 2)
+// {
+// Console.WriteLine("Вторник");
+// }
+// if (day == 3)
+// {
+// Console.WriteLine("Среда");
+// }
+// if (day == 4)
+// {
+// Console.WriteLine("Четверг");
+// }
+// if (day == 5)
+// {
+// Console.WriteLine("Пятница");
+// }
+// if (day == 6)
+// {
+// Console.WriteLine("Суббота");
+// }
+// if (day == 7)
+// {
+// Console.WriteLine("Воскресенье");
+// }
+// }
+// else
+// {
+// Console.WriteLine("Это не день недели");
+// }
+
+
+switch (day)
+{
+case 1:
+{
+Console.WriteLine("Понедельник");
+break;
+}
+case 2:
+{
+Console.WriteLine("Вторник");
+break;
+}
+case 3:
+{
+Console.WriteLine("Среда");
+break;
+}
+case 4:
+{
+Console.WriteLine("Четверг");
+break;
+}
+case 5:
+{
+Console.WriteLine("Пятница");
+break;
+}
+case 6:
+{
+Console.WriteLine("Суббота");
+break;
+}
+case 7:
+{
+Console.WriteLine("Воскресенье");
+break;
+}
+default:
+{
+Console.WriteLine("Это не день недели");
+break;
+}
+}
+
