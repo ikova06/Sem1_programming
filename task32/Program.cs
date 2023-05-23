99a3d7af99cbf18b61ffb667c3ab12d3d395ea46
ikova06 <ikova06#gmail.com>
2023-05-23 07:58:08 +0300

    Added hometask to Sem4

--- task32/Program.cs
+++ task32/Program.cs
@@ -1,5 +1,29 @@
 ﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
-// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
-// 4; массив [6, 7, 19, 345, 3] -> нет
-// -3; массив [6, 7, 19, 345, 3] -> да
+
+
+int[] FillArrayWithRandomNumber(int size)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(-9, 10);
+}
+return arr;
+}
+
+int[] ChangeArray(int[] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] *=-1;
+}
+return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+System.Console.WriteLine(string.Join(", ", ChangeArray(array)));
---
+++ task33/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 33: Задайте массив. 
+// Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] FillArrayWithRandomNumber(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+    return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+
+System.Console.Write("Введите искомое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == number)
+{
+    Console.WriteLine ("Ваше число есть в массиве");
+    break;
+}
+else 
+{
+    Console.WriteLine ("Вашего числа в массиве нет");
+    break;
+}
+}
+
