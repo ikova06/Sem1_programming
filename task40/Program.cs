7d7804960047d5c03ba21fc4886a53c62ef4df7e
ikova06 <ikova06#gmail.com>
2023-05-29 22:57:27 +0300

    Added task to Sem6

---
+++ task40/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник со сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.WriteLine("Введите сторону A ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону B ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону C ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (numberA + numberB > numberC &&  numberC + numberB > numberA && numberC + numberA > numberB)
+{
+    Console.WriteLine($"Треугольник такой длинны может существовать");
+}
+else 
+{
+    Console.WriteLine($"Треугольник такой длинны не может существовать");
+}
---
+++ task45/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 45: Напишите программу, 
+// которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] FillArrayWithRandomNumber(int size)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(1,11);
+}
+return arr;
+}
+
+int[] MethodCopyArray(int length, int[] array)
+{
+int[] arraycopy = new int[length];
+for (int j = 0; j < length; j++)
+{
+arraycopy[j] = array[j];
+}
+return arraycopy;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+
+int[] res = MethodCopyArray(length, array);
+System.Console.WriteLine(string.Join(", ", res));
