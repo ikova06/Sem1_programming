02269dadcc9b2847332227822e99287f193dbcda
ikova06 <ikova06#gmail.com>
2023-06-02 23:36:07 +0300

    Added task to Sem7

---
+++ hometask47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void FillMatrixWhithRandomNumbers(double[,] matrix)
+{
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = Math.Round(rnd.NextDouble (), 3);
+}
+}
+}
+
+void PrintMatrix(double[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+System.Console.Write($"{matrix[i, j]} " + "\t" + "\t");
+}
+System.Console.WriteLine();
+}
+}
+
+System.Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[m, n];
+
+FillMatrixWhithRandomNumbers(matrix);
+PrintMatrix(matrix);
---
+++ hometask50/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void FillMatrixWhithRandomNumbers(int[,] matrix)
+{
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = rnd.Next(0, 11);
+Console.Write($"{matrix[i, j]} " + "\t");
+}
+Console.WriteLine();
+}
+}
+
+
+// void PrintMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// System.Console.Write($"{matrix[i, j]} " + "\t");
+// }
+// System.Console.WriteLine();
+// }
+// }
+
+void FindArrayElement(int [,] element, int userindex)
+{
+    bool find = false;
+    for (int i = 0; i < element.GetLength(0); i++)
+{   
+for (int j = 0; j < element.GetLength(1); j++)
+{
+if (element [i, j] == userindex)
+Console.WriteLine("Индекс заданного числа "+ i + " " +j);
+find = true;
+}
+}
+if (!find)
+Console.WriteLine("Такого числа нет в массиве");
+}
+
+System.Console.Write("Введите строку массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите столбец массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+FillMatrixWhithRandomNumbers(matrix);
+// PrintMatrix(matrix);
+FindArrayElement(matrix, a);
